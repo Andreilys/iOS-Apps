48c70b2d3009883af7e0227b090d424472142a97
Andreilys <andreilyskov@gmail.com>
2015-07-01 23:08:22 +0800

    trying out google map api

---
+++ googleMapAPI/Pods/Target Support Files/Pods-googleMapAPITests/Pods-googleMapAPITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_googleMapAPITests : NSObject
+@end
+@implementation PodsDummy_Pods_googleMapAPITests
+@end
---
+++ googleMapAPI/googleMapAPI/AppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.h
+//  googleMapAPI
+//
+//  Created by Andrei on 7/1/15.
+//  Copyright (c) 2015 Andrei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+@import GoogleMaps;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
