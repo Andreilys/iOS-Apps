3d19e8b7a666e1c14fd263ca648e5c75664937a7
Andreilys <andreilyskov@gmail.com>
2015-07-05 21:04:50 +0800

    trying out a new app idea

---
+++ habitChecker/habitChecker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  habitChecker
+//
+//  Created by Andrei on 7/5/15.
+//  Copyright (c) 2015 Andrei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ habitChecker/habitChecker/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  habitChecker
+//
+//  Created by Andrei on 7/5/15.
+//  Copyright (c) 2015 Andrei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
