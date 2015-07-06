1cde91186e28f119cd311c32f6ddb0b9f070e330
Andreilys <andreilyskov@gmail.com>
2015-07-06 21:06:57 +0800

    trying out new app

--- habitChecker/habitChecker/ViewController.m
+++ habitChecker/habitChecker/ViewController.m
@@ -7,12 +7,20 @@
 //
 
 #import "ViewController.h"
+#import "UIKit/UIKit.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIButton *whiteButton1;
+@property (weak, nonatomic) IBOutlet UIButton *whiteButton2;
+@property (weak, nonatomic) IBOutlet UIButton *whiteButton3;
+@property (weak, nonatomic) IBOutlet UIButton *whiteButton4;
 
 @end
 
 @implementation ViewController
+{
+    int count;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -23,5 +31,23 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+- (IBAction)turnGreen:(id)sender {
+    UIButton *button = sender;
+    button.backgroundColor = [UIColor greenColor];
+    count++;
+    if (count == 4) {
+        NSLog(@"works");
+    }
+}
+
+- (IBAction)reset:(id)sender {
+    _whiteButton1.backgroundColor = [UIColor whiteColor];
+    _whiteButton2.backgroundColor = [UIColor whiteColor];
+    _whiteButton3.backgroundColor = [UIColor whiteColor];
+    _whiteButton4.backgroundColor = [UIColor whiteColor];
+    count = 0;
+}
+
+
 
 @end
