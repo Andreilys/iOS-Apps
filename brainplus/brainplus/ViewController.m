3e6718657dcbddd430a38672ff44cb7e64fa01a4
Andreilys <andreilyskov@gmail.com>
2015-07-15 21:36:13 +0800

    brainplus

---
+++ brainplus/brainplus/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  brainplus
+//
+//  Created by Andrei on 7/10/15.
+//  Copyright (c) 2015 Andrei. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIImageView *pic1;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
